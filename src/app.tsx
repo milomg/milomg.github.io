7b5e9a78bf963c00b45e8d5fb84fe030b34f1c15
Milo <milomg4@gmail.com>
2024-06-30 15:36:33 -0700

    fix meta provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,18 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
+import { MetaProvider } from "@solidjs/meta";
 import { Suspense } from "solid-js";
 import "./style.css";
 
 export default function App() {
   return (
-    <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+    <Router
+      root={(props) => (
+        <MetaProvider>
+          <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+      )}
+    >
       <FileRoutes />
     </Router>
   );
